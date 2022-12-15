77de12e1afa227f7294aa465a63be385197ec90a
NguyenVinhThien <113451601+NguyenVinhThien@users.noreply.github.com>
2022-12-16 00:45:51 +0700

    Update Main.java

--- projectCNTT/src/ITproject/Main.java
+++ projectCNTT/src/ITproject/Main.java
@@ -11,11 +11,10 @@ public class Main {
 	 public static void main(String[] args) {
 
 	        String hex = Bits.stringToHex("Dai hoc Su pham ky thuat thanh pho Ho Chi Minh");
-	        //hex = 48 65 6C 6C 6F 20 57 6F 72 6C 64 21
 	        Message msg = new Message(hex);
 	        System.out.println(msg.toString());
 
-                    Key key = new Key("6D62B676B5F7B932");
+                Key key = new Key("6D62B676B5F7B932");
 
 	        DES des = new DES();
 	        Message enc = des.encrypt(msg, key);
